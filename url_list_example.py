41a31c68714d7bd3258198602085979ac5f94c5d
icarus44_zer0 <jpoe@csus.edu>
2021-10-20 14:51:29 -0700

    sample data type

---
+++ url_list_example.py
@@ -0,0 +1,4 @@
+parts: list(dict) = [
+    {'dir': 'Chapter1', 'name': 'Part1', 'presentation': '317987e80fd74ecea77849d96be8c4941d',
+        'playbackTicket': '5cd78ea60bf64d39935b6d63db2da740'},
+]
