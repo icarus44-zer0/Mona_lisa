a09ff89db0032ce573ccab5e8177c1b85421b87a
icarus44_zer0 <jpoe@csus.edu>
2021-10-20 14:50:29 -0700

    algorithm performance enhancements

--- mona_lisa.py
+++ mona_lisa.py
@@ -1,22 +1,53 @@
-import requests 
+import typing
+import requests
 import shutil
+import os
+from typing import List, Dict, Any
 
-def main():
 
-    inc: int = 0
-
-    parts: list(dict) = [
-        {'dir': '', 'name': '', 'presentation': '','playbackTicket': ''},
-        {'dir': '', 'name': '', 'presentation': '','playbackTicket': ''},    
-        {'dir': '', 'name': '', 'presentation': '','playbackTicket': ''},    
-        {'dir': '', 'name': '', 'presentation': '','playbackTicket': ''},    
-        {'dir': '', 'name': '', 'presentation': '','playbackTicket': ''},    
-        {'dir': '', 'name': '', 'presentation': '','playbackTicket': ''},    
-        {'dir': '', 'name': '', 'presentation': '','playbackTicket': ''},    
-        {'dir': '', 'name': '', 'presentation': '','playbackTicket': ''},    
-        {'dir': '', 'name': '', 'presentation': '','playbackTicket': ''},    
-        {'dir': '', 'name': '', 'presentation': '','playbackTicket': ''},
+def example():
+    example_parts: List[Dict] = [
+        {'dir': '', 'name': '', 'presentation': '', 'playbackTicket': ''},
+        {'dir': '', 'name': '', 'presentation': '', 'playbackTicket': ''},
+        {'dir': '', 'name': '', 'presentation': '', 'playbackTicket': ''},
     ]
+
+
+def make_dir(parentdir, dirname):
+    try:
+        path = os.path.join(parentdir, dirname)
+        os.mkdir(path)
+    except:
+        return
+
+
+def make_parts(parts: List[Dict], dir: str, name: str) -> List[Dict]:
+
+    p_offset: int = 96  # presentation/
+    p_offset2: int = 130  # id len
+    t_offset: int = 169  # playbackTicket/
+    t_offset2: int = 201  # id len (EOL would be simpler)
+    count = 0
+
+    with open('urls.txt') as f:
+        lines = f.readlines()
+
+    for line in range(len(lines)):
+        count += 1
+        string = lines[line]
+
+        pres = string[p_offset: p_offset2]
+        ticket = string[t_offset: t_offset2]
+
+        temp: dict = {'dir': dir, 'name': name + str(count),
+                      'presentation': pres, 'playbackTicket': ticket}
+        parts.append(temp)
+
+    return parts
+
+
+def get_slides(parts: List[Dict]):
+    inc: int = 0
 
     for i in range(len(parts)):
         seperator = '_'
@@ -37,28 +68,38 @@ def main():
                 slide = 'slide_' + slide
 
                 image_url = (
-                    "https://csus.mediasite.com/Mediasite/FileServer/f91cef6bec4e43d297b3859d0d54b19d29/Presentation/%(presentation)s/%(slide)s.jpg?playbackTicket=%(playbackTicket)s"
+                    "https://csus.mediasite.com/Mediasite/FileServer/f91cef6bec4e43d297b3859d0d54b19d29/Presentation/%(presentation)s/%(slide)s_819_512.jpg?playbackTicket=%(playbackTicket)s"
                     % locals())
 
+                print(image_url)
                 route = image_url.split("/")[-1]
                 params = route.split("?", 1)
                 slide_name = params[0]
 
                 # Open the url image, set stream to True, this will return the stream content.
-                r = requests.get(image_url, stream=True)
-                if r.status_code != 200:
-                    print("Image Couldn't be retreived\n")
-                    print(image_url)
+                try:
+                    r = requests.get(image_url, stream=True)
+                    if r.status_code != 200:
+                        raise Exception('End of slides')
+                    else:
+                        r.raw.decode_content = True
+                        with open('./' + dir_name + '/' + dir_name + seperator + part_name + seperator + slide_name, "wb") as f:
+                            shutil.copyfileobj(r.raw, f)
+                except Exception as er:
                     r.close()
+                    print(er)
                     break
-                elif r.status_code == 200:
-                    r.raw.decode_content = True
-                    with open('./'+ dir_name + '/' + dir_name + seperator + part_name + seperator + slide_name, "wb") as f:
-                        shutil.copyfileobj(r.raw, f)
-                else:
-                    print("Error: " + str(r.status_code))
-                    r.close()
-                    break
+
+
+def main():
+    parts: List[Dict] = []
+    parentdir = '.'
+    dirname = 'Chapter6'
+    filename = 'part'
+    make_dir(parentdir=parentdir, dirname=dirname)
+    parts = make_parts(parts=parts, dir=dirname, name=filename)
+    get_slides(parts)
+
 
 if __name__ == "__main__":
     main()
